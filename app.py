bf545e540102b922517107d3f3ed49f1ce81503f
devin-baxter <baxter.devin@gmail.com>
2020-11-30 23:07:44 -0800

    Register Login and Dashboard all work

--- app.py
+++ app.py
@@ -65,11 +65,45 @@ def register():
 		# Flash Message
 		flash('Registration Successful', 'success')
 		redirect(url_for('home'))
-		return redirect(url_for('login.html'))
+		return redirect(url_for('login'))
 	return render_template('register.html', form=form)
 
-@app.route('/login', methods='POST')
-def login()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		# Get Form Fields
+		username = request.form['username']
+		password_candidate = request.form['password']
+		# Create Cursor and MySQL Connection
+		cur = mysql.connection.cursor()
+		# Get user via username
+		result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+		if result > 0:
+			# Get stored hash
+			data = cur.fetchone()
+			password = data['password']
+			# Compare Passwords
+			if sha256_crypt.verify(password_candidate, password):
+				# Successful Login
+				session['logged_in'] = True
+				session['username'] = username
+				flash('You are logged in', 'success')
+				return redirect(url_for('dashboard'))
+			else:
+				error = 'Invalid login'
+				app.logger.info('INVALID LOGIN ATTEMPT')
+				return render_template('login.html', error=error)
+			# Close Connection
+			cur.close()
+		else:
+			error = 'Username not found'
+			return render_template('login.html', error=error)
+	return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
 	app.secret_key='12345'
